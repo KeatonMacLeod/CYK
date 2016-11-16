84912a8db446e2677b14da9973ab5d57ce707138
Keaton MacLeod <macleodkeaton@gmail.com>
2016-11-16 12:37:24 -0600

    Modified files...

---
+++ CYK/src/Cell.java
@@ -0,0 +1,12 @@
+public class Cell {
+	
+	private String id;
+	private String sets;
+	
+	public Cell (String identification, String setVariables)
+	{
+		id = identification;
+		sets = setVariables;
+	}
+	
+}

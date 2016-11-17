4ec98646035b4c93c3f309b12cc838c56db583e7
Keaton MacLeod <macleodkeaton@gmail.com>
2016-11-16 19:17:45 -0600

    Added new methods.

--- CYK/src/Cell.java
+++ CYK/src/Cell.java
@@ -9,4 +9,31 @@ public class Cell {
 		sets = setVariables;
 	}
 	
+	public String getId()
+	{
+		return id;
+	}
+	
+	public String getSets()
+	{
+		return sets;
+	}
+	
+	public void print()
+	{
+		String sets = this.getSets();
+		System.out.print(this.getId() + "->");
+		for (int i = 0; i < sets.length(); i++)
+		{
+			if (i+1 == sets.length())
+			{
+				System.out.println(sets.charAt(i));
+			}
+			else
+			{
+				System.out.print(sets.charAt(i) + "|");
+			}
+		}
+	}
+	
 }
